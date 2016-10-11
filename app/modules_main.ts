e63748f916ac5761b14f675bf79a1f31fce92e84
Zied Kharraz <zied.kharraz@gmail.com>
2016-10-11 15:24:24 +0100

    MetaData

--- app/modules_main.ts
+++
@@ -1,4 +0,0 @@
-import {bootstrap} from "angular2/platform/browser"     //importing bootstrap function
-import {MyModulesClass} from "./modules_app.component"  //importing component function
-
-bootstrap(MyModulesClass);
