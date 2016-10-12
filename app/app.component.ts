fd064c5536606fcfc1feb837a29e52f7a1c64b0c
Zied Kharraz <zied.kharraz@gmail.com>
2016-10-12 23:53:42 +0200

    Start tuto officiel angular

---
+++ app/app.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'my-app',
+  template: '<h1>My First Angular App 7</h1>'
+})
+export class AppComponent { }
--- app/attribute_main.ts
+++
@@ -1,4 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';       //importing bootstrap function
-import {AppComponent} from "./attribute_app.component";    //importing component function
-
-bootstrap(AppComponent);
