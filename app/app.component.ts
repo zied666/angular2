68e619a7a812ce937b24e53182791d0c0a91d80e
Zied Kharraz <zied.kharraz@gmail.com>
2016-10-14 11:31:11 +0100

    complete ng Module

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,24 @@
 import { Component } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
-    selector: 'my-app',
-    template: `
-    <h1>{{title}}</h1>
+  selector: 'my-app',
+  template: `
+    <app-title [subtitle]="subtitle"></app-title>
     <nav>
-      <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
-      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+      <a routerLink="contact" routerLinkActive="active">Contact</a>
+      <a routerLink="crisis"  routerLinkActive="active">Crisis Center</a>
+      <a routerLink="heroes"  routerLinkActive="active">Heroes</a>
     </nav>
     <router-outlet></router-outlet>
-  `,
-    styleUrls: ['app.component.css'],
+  `
 })
 export class AppComponent {
-    title = 'Tour of Heroes';
+  subtitle = '(Final)';
 }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,38 +1,34 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
-import { HttpModule }    from '@angular/http';
-import './rxjs-extensions';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+
+/* App Root */
+import { AppComponent }   from './app.component';
+
+/* Feature Modules */
+import { ContactModule }    from './contact/contact.module';
+import { CoreModule }       from './core/core.module';
+
+/* Routing Module */
 import { AppRoutingModule } from './app-routing.module';
 
-// Imports for loading & configuring the in-memory web api
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api/in-memory-web-api.module';
-import { InMemoryDataService }  from './in-memory-data.service';
-
-import { AppComponent }         from './app.component';
-import { DashboardComponent }   from './dashboard.component';
-import { HeroesComponent }      from './heroes.component';
-import { HeroDetailComponent }  from './hero-detail.component';
-import { HeroService }          from './hero.service';
-import {HeroSearchComponent} from "./hero-search.component";
-
 @NgModule({
-    imports: [
-        BrowserModule,
-        FormsModule,
-        HttpModule,
-        InMemoryWebApiModule.forRoot(InMemoryDataService),
-        AppRoutingModule
-    ],
-    declarations: [
-        AppComponent,
-        DashboardComponent,
-        HeroDetailComponent,
-        HeroesComponent,
-        HeroSearchComponent
-    ],
-
-    providers: [ HeroService ],
-    bootstrap: [ AppComponent ]
+  imports: [
+    BrowserModule,
+    ContactModule,
+/*
+    CoreModule,
+*/
+    CoreModule.forRoot({userName: 'Miss Marple'}),
+    AppRoutingModule
+  ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
+
+
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
